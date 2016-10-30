1b6e25c33db0082f702a8deda19a2e80986979da
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2016-10-29 20:04:43 -0400

    Trying to push scraped data into an array

--- scr.js
+++ scr.js
@@ -5,12 +5,12 @@ var readableStream = fs.createReadStream('urls.txt');
 var data = '';
 readableStream.setEncoding('utf8');
 
-//var selector = '.geo';
 var url;
 var model = {
   'name': '.firstHeading',
   'location':{ selector: '.geo', unique: true}
 };
+var locations = [];
 
 readableStream.on('data', function(chunk){
   data+=chunk;
@@ -19,16 +19,19 @@ readableStream.on('data', function(chunk){
 readableStream.on('end', function(){
   //console.log(data);
   var list = data.split(',');
-  console.log(list[0]);
+  //console.log(list[0]);
 
   for (var i = 0; i < list.length; i++) {
     url = list[i];
 
     scrapy.scrape(url, model, function(err, data) {
-        if (err) return console.error(err)
-        console.log(data)
+        if (err) return console.error(err);
+        //console.log(data);
+        locations.push(data)
     })
 
   }
 
 })
+
+console.log(locations)
