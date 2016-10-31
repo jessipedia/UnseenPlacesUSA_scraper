f4e20229d6c617f5b09c7587d696e92c4fd8fee0
Jessica Scott-Dutcher <jscottdutcher@gmail.com>
2016-10-31 18:07:15 -0400

    Add write JSON to file.

--- scr.js
+++ scr.js
@@ -19,13 +19,27 @@ readableStream.on('data', function(chunk){
 readableStream.on('end', function(){
   //console.log(data);
   var list = data.split(',');
+  fs.writeFile('data.json')
+
   //console.log(list[0]);
 
   for (var i = 0; i < list.length; i++) {
     url = list[i];
+    console.log(url);
 
     scrapy.scrape(url, model, function(err, data) {
         if (err) return console.error(err);
+
+        else{
+          var json = JSON.stringify(data, null, 2);
+            //fs.writeFile('data.json', json, 'utf8', finished);
+            fs.appendFile('data.json', '\n\n' + json, finished);
+            function finished(err) {
+            console.log('Finished writing additional.json')
+            console.log(data);
+            }
+        }
+
         //console.log(data);
         locations.push(data)
     })
